e698cc05f03db2a64fa26416efdda07dba278a69
Your Name <you@example.com>
2025-02-06 14:30:56 +0900

    회원가입 페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,6 +7,7 @@ const ctrl = require("./home.ctrl");
 
 routers.get("/", ctrl.output.home);
 routers.get("/login", ctrl.output.login);
+routers.get("/register", ctrl.output.register);
 routers.post("/login", ctrl.process.login);
 
 module.exports = routers;
